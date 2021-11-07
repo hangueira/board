3066973a4e1d19405ef9f7b3d01adc785930cde9
hangueira <77866502+hangueira@users.noreply.github.com>
2021-11-07 15:25:51 +0900

    init

---
+++ src/test/java/com/lovemin/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovemin.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
